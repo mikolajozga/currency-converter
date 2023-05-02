8286b99ddc2002a7dd09dae5d59ed1c38fed4bb1
Mikolaj Ozga <mikolaj.ozga03@gmail.com>
2023-05-02 16:52:47 +0200

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,47 @@
+{
+    const calculateResult = (amount, currency) => {
+        const rateEUR = 4.5776
+        const rateUSD = 4.1712
+        const rateGBP = 5.2052
+
+        switch (currency) {
+            case "EUR":
+                return amount / rateEUR;
+
+            case "USD":
+                return amount / rateUSD;
+
+            case "GBP":
+                return amount / rateGBP;
+
+        }
+    };
+
+    const updateResultText = (amount, result, currency) => {
+        const resultElement = document.querySelector(".js-result");
+        resultElement.innerHTML = `${amount.toFixed(2)} PLN = <strong>${result.toFixed(2)} ${currency}</strong>`;
+    }
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+
+        const amountElement = document.querySelector(".js-amount");
+        const currencyRateElement = document.querySelector(".js-currencyRate");
+
+        const amount = +amountElement.value;
+        const currency = currencyRateElement.value;
+
+        const result = calculateResult(amount, currency);
+
+        updateResultText(amount, result, currency);
+
+    };
+
+    const init = () => {
+        const formElement = document.querySelector(".js-form");
+
+        formElement.addEventListener("submit", onFormSubmit);
+    };
+
+    init();
+};
